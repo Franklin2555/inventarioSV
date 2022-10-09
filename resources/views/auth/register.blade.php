c8316745244cb0821a2d123433e326382a2cf848
ÁlvaroFloresHome <moralesalvaro308@gmail.com>
2022-10-09 11:28:00 -0600

    Formulario de registro de usuarios v1

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,64 +1,69 @@
 <x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+      <a href="{{url('/dashboard')}}" class="h1"><b>Inventario</b>SV</a>
+    </div>
+    <div class="card-body">
+      <p class="login-box-msg">Registrar un nuevo usuario</p>
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <!-- Name -->
-            <div>
-                <x-input-label for="name" :value="__('Name')" />
-
-                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-
-                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+      <form method="POST" action="{{ route('register') }}">
+        @csrf
+        
+        <div class="input-group mb-3">
+          <input type="text" id="name" name="name" class="form-control" placeholder="Nombre completo">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
             </div>
-
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-input-label for="email" :value="__('Email')" />
-
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="email" name="email" id="email" class="form-control" placeholder="Correo electrónico">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
             </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
-
-                <x-text-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" name="password" id="password" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
             </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-
-                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" placeholder="Retype password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-primary-button class="ml-4">
-                    {{ __('Register') }}
-                </x-primary-button>
+            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" id="agreeTerms" name="terms" value="agree">
+              <label for="agreeTerms">
+               He leído los <a href="#">términos y condiciones</a>
+              </label>
             </div>
-        </form>
-    </x-auth-card>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Registrar</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+      <a href="{{url('login')}}" class="text-center">Ya tengo una cuenta</a>
+    </div>
+    <!-- /.form-box -->
+  </div><!-- /.card -->
 </x-guest-layout>
